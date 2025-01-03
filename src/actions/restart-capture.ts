bf389cd4248b3bdea5a6a0ec4911a45e321d5dfe
PhiMaii <pmairguenther@web.de>
2025-01-03 23:01:54 +0100

    Implemented a working restart capture button

--- src/actions/restart-capture.ts
+++ src/actions/restart-capture.ts
@@ -1,19 +1,40 @@
-import streamDeck, { action, type KeyDownEvent, SingletonAction } from "@elgato/streamdeck";
+import streamDeck, {
+  action,
+  type KeyDownEvent,
+  SingletonAction,
+} from "@elgato/streamdeck";
+import { time } from "console";
 /**
  * An action that logs a Stream Deck key press.
  */
 
 @action({ UUID: "net.phimai.hyperion-screen-capture-remote.restart-capture" })
 export class RestartCapture extends SingletonAction {
-	/**
-	 * Handles the user pressing a Stream Deck key (pedal, G-key, etc).
-	 * @param ev Information about the event.
-	 */
-	override onKeyDown(ev: KeyDownEvent<Settings>): void | Promise<void> {
-		streamDeck.logger.info(`Restart Key pressed!`);
-	}
+  /**
+   * Handles the user pressing a Stream Deck key (pedal, G-key, etc).
+   * @param ev Information about the event.
+   */
+
+  async onKeyDown(ev: KeyDownEvent<CounterSettings>): Promise<void> {
+    const onCommandUrl = "http://localhost:9191/?command=ON";
+    const offCommandUrl = "http://localhost:9191/?command=OFF";
+
+    try {
+      await fetch(offCommandUrl);
+    } catch (error) {
+      streamDeck.logger.error("Error!");
+    }
+    streamDeck.logger.info("Succesfully sent command");
+
+    try {
+      await fetch(onCommandUrl);
+    } catch (error) {
+      streamDeck.logger.error("Error!");
+    }
+    streamDeck.logger.info("Succesfully sent command");
+  }
 }
 
-
-type Settings = {
+type CounterSettings = {
+  count: number;
 };
